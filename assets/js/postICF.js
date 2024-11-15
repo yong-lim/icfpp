65ac3e05edbc2ebff1b6a9a2264facd5f035fd03
Yong Lim <yong.lim@gmail.com>
2024-11-15 19:25:25 +0700

    Add contact form to Home Group page

---
+++ assets/js/postICF.js
@@ -0,0 +1,59 @@
+function formFocus() {
+  $('#alert-field').hide().addClass('hidden');
+}
+
+function formReset() {
+  $('#alert-field').show()
+    .html("<span><p>Thank you for dropping us a line . . .</p></span><br>");
+  document.getElementById("formID").reset();
+  setTimeout(() => {
+    $('#sendButton').show();
+    $('#alert-field').hide();
+    console.log('This alert appeared after 3 second!'); 
+  }, 3000);
+}
+
+function pleaseWaite() {
+  console.log('hide sendButton'); 
+  $('#sendButton').hide();
+  $('#alert-field').removeClass();
+  console.log('show alert-field'); 
+  $('#alert-field').show()
+    .html("<div><p>Please wait while we're sending your message . . .</p>  <progress></progress></div>");
+}
+
+//selector from your HTML form
+function postICF(e) {
+  //prevent the form from submiting so we can post to the google form
+  e.preventDefault();
+  // console.log("inpostEatery");
+  pleaseWaite();
+
+  //AJAX request
+  $.ajax({
+    //The public Google Form url, but replace /view with /formResponse
+    // url: 'https://docs.google.com/forms/d/e/1FAIpQLSf6Paftyz8sBWHuG9TfaWcspP8LN0Mw-0ubF6KyGu3b9NpGGQ/formResponse',     
+    url: 'https://docs.google.com/forms/d/e/1FAIpQLScZ47fKGFJPtLN6bcraqZiZBq8WOcZ6mEpyRD5STR_-D1QqNg/formResponse',
+    data: $('#formID').serialize(), //Nifty jquery function that gets all the input data 
+    type: 'POST', //tells ajax to post the data to the url
+    dataType: "json", //the standard data type for most ajax requests
+    mode: 'cors',
+    statusCode: { //the status code from the POST request
+      0: function(data) { //0 is when Google gives a CORS error, don't worry it went through
+        //success
+        formReset();
+       }, 
+       200: function(data) {//200 is a success code. it went through!
+        //success
+        // $('#form-success').text('hooray! 200');
+        formReset();
+       },
+       403: function(data) {//403 is when something went wrong and the submission didn't go through
+        //error
+        $('#alert-field').show()
+          .html("<span><p><b>Oh no! something went wrong. Please let us know of your problem.</b></p></span>");
+        alert('Oh no! something went wrong. Please let us know of your problem.');
+      }
+    }  
+  });
+}
--- home-group.md
+++ home-group.md
@@ -8,4 +8,7 @@ page-banner: /assets/images/lunch-1.jpg
 
 Want to try hosting, leading or attending a Home Group? Our Home Group Kickstart is starting soon! This is a 4-week initiative with the option to continue longer term. Study materials will be provided! Home Groups are a life-giving part of community and this is the time to prayerfully consider participating.
 
-If you are interested in being part of a Home Group and/or leading or hosting one please contact Christina at [admin@icfpp.org](mailto:admin@icfpp.org) for more information.
+If you are interested in being part of a Home Group and/or leading or hosting one please contact us for more information.
+
+{%- include contact.html -%}
+
